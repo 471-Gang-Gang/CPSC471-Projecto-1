d34c2da13351b996bfd7b37168116e98a76d9620
Sagar Joshi <sagarj0698@gmail.com>
2021-02-25 17:02:17 -0800

    can you dig it

--- web_server.py
+++ web_server.py
@@ -6,31 +6,31 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 # TASK 1
 #Fill in start
-
+serverPort = 45678
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
+print('the web server is up on port:', serverPort)
 #Fill in end
 
 while True:
    #Establish the connection
    print('Ready to serve...')
-
    # TASK 2
-   connectionSocket, addr = #Fill in start  #Fill in end
+   connectionSocket, addr = serverSocket.accept()
 
    try:
-
       # TASK 3
-      message = #Fill in start      #Fill in end
-
+      message = connectionSocket.recv(1024)
+      print('Message: ', message)  #testing 
       filename = message.split()[1]
-      f = open(filename[1:])
-
-      # TASK 4
-      outputdata = #Fill in start       #Fill in end
-
+      print('Filename: ', filename) #testing filename
+      f = open(filename[1:],"r")
+      outputdata = f.read()
+      # print(outputdata)
       # TASK 5
       #Send one HTTP header line into socket
       #Fill in start
-      
+      connectionSocket.send(bytes("HTTP/1.1 200 OK\r\n\r\n","UTF-8"))
       #Fill in end
 
       #Send the content of the requested file to the client
@@ -40,17 +40,20 @@ while True:
       connectionSocket.close()
    except IOError:
 
+
       # TASK 6
       #Send response message for file not found
       #Fill in start
+      connectionSocket.send(bytes("\nHTTP/1.1 404 Not Found\n\n", "UTF-8"))
       
       #Fill in end
 
       # TASK 7
       #Close client socket
       #Fill in start
-      
+      connectionSocket.send(bytes("<html><head></head><body><h1>404 Not Found</h1></body></html>","UTF-8"))
+      connectionSocket.close()
       #Fill in end
 
 serverSocket.close()
-sys.exit()#Terminate the program after sending the corresponding data
+sys.exit()   #Terminate the program after sending the corresponding data
